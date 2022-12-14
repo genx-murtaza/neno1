76490fe45b1961b08f4b7cf813a90628c1dc5fb7
Murtaza <murtazask@yahoo.com>
2022-12-14 12:34:06 +0300

    first neno1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -62,10 +62,10 @@
                         >
                             <i class="bx bx-dots-vertical-rounded"></i>
                         </button>
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                        {{-- <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
                             <a class="dropdown-item" href="javascript:void(0);">View More</a>
                             <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                        </div>
+                        </div> --}}
                         </div>
                     </div>
                     <span class="fw-semibold d-block mb-1">Profit</span>
